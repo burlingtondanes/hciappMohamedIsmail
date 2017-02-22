9f91144db8beb5ceaabd2bbfa00e219428ea6a4a
Unknown <Mohamed.ismailbda@gmail.com>
2017-02-22 12:44:45 +0000

    update via codepn

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function runOCR(url) {
+    Tesseract.recognize(url)
+         .then(function(result) {
+            document.getElementById("ocr_results")
+                    .innerText = result.text;
+         }).progress(function(result) {
+            document.getElementById("ocr_status")
+                    .innerText = result["status"] + " (" +
+                        (result["progress"] * 100) + "%)";
+        });
+}
+
+document.getElementById("go_button")
+        .addEventListener("click", function(e) {
+            var url = document.getElementById("url").value;
+            runOCR(url);
+        });
